4525f9c92c4f7f69ced4ecf8debeec65be6b9da0
Liam Riddell <web@liamriddell.co.uk>
2020-06-22 23:22:23 +0100

    https://github.com/mjmlio/mjml/blob/master/packages/mjml-core/src/helpers/conditionalTag.js

---
+++ MJML/Components/MjmlTextComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Mjml.Components
+{
+    public class MjmlTextComponent : MjmlBaseComponent
+    {
+        public MjmlTextComponent(XElement element) : base(element)
+        {
+        }
+
+        public override string RenderMjml()
+        {
+            return $@"
+            <MjmlRawComponent type=""{Element.Name.LocalName}"">
+                {this.RenderChildren()}
+            </MjmlRawComponent>";
+        }
+    }
+}
---
+++ MJML/Helpers/TagHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mjml.Helpers
+{
+    public static class TagHelpers
+    {
+        private const string startConditionalTag = "<!--[if mso | IE]>";
+        private const string startMsoConditionalTag = "<!--[if mso]>";
+        private const string endConditionalTag = "<![endif]-->";
+        private const string startNegationConditionalTag = "<!--[if !mso | IE]><!-->";
+        private const string startMsoNegationConditionalTag = "<!--[if !mso><!-->";
+        private const string endNegationConditionalTag = "<!--<![endif]-->";
+
+        public static string ConditionalTag(string content, bool negation = false)
+        {
+            return $@"
+                {(negation ? startNegationConditionalTag : startConditionalTag)}
+                {content}
+                {(negation ? endNegationConditionalTag : endConditionalTag)}
+            ";
+        }
+
+        public static string MsoConditionalTag(string content, bool negation = false)
+        {
+            return $@"
+                {(negation ? startMsoNegationConditionalTag : startMsoConditionalTag)}
+                {content}
+                {(negation ? endNegationConditionalTag : endConditionalTag)}
+            ";
+        }
+    }
+}
