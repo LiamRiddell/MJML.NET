8ae24986eb85663d26371a8a83ed65073158db9a
Liam Riddell <web@liamriddell.co.uk>
2020-06-22 22:52:15 +0100

    Removed unecessary code

--- MjmlConsoleTest/Program.cs
+++ MjmlConsoleTest/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Mjml;
 
 namespace MjmlConsoleTest
@@ -11,7 +7,30 @@ namespace MjmlConsoleTest
     {
         private static void Main(string[] args)
         {
-            MjmlParserTest.Parse();
+            string mjmlString = @"
+                <mjml>
+                    <mj-head>
+                        <mj-breakpoint width=""320px"" />
+                    </mj-head>
+                    <mj-body>
+                        <mj-section>
+                            <mj-column>
+                                <mj-text font-size="" 20px"" color="" #F45E43"" font-family="" helvetica"">Hello World</mj-text>
+                                <mj-divider border-color="" #F45E43""></mj-divider>
+                            </mj-column>
+                        </mj-section>
+                    </mj-body>
+                </mjml>
+            ";
+
+            var mjmlParser = new MjmlParser();
+
+            if (!mjmlParser.TryParse(mjmlString, new { }, out var html))
+            {
+                // ... handle failure
+            }
+
+            Console.WriteLine(html);
         }
     }
 }
