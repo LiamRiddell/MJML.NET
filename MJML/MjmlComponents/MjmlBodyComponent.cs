acdcefdf5d0eb1ae7da22d81e05fbe280b9b7151
Liam Riddell <web@liamriddell.co.uk>
2020-07-06 21:46:51 +0100

    Added mj-body

---
+++ MJML/MjmlComponents/MjmlBodyComponent.cs
@@ -0,0 +1,45 @@
+﻿using Mjml.Core;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace Mjml.MjmlComponents
+{
+    public class MjmlBodyComponent : BodyComponent
+    {
+        public MjmlBodyComponent(XElement element) : base(element)
+        {
+        }
+
+        public override Dictionary<string, string> SetAllowedAttributes()
+        {
+            return new Dictionary<string, string>
+            {
+                { "width", "600px" },
+                { "background-color", string.Empty }
+            };
+        }
+
+        public override void SetupStyles()
+        {
+            StyleLibraries.AddStyleLibrary("div", new Dictionary<string, string>() {
+                { "background-color", GetAttribute("background-color") },
+            });
+        }
+
+        public override string RenderMjml()
+        {
+            string backgroundColor = GetAttribute("background-color");
+            // TODO: setBackgroundColor(this.getAttribute('background-color'))
+
+            return $@"
+                <div {HtmlAttributes(new Dictionary<string, string> {
+                        { "class", GetAttribute("css-class") },
+                        { "style", "div" }
+                     })}
+                >
+                    {this.RenderChildren()}
+                </div>
+            ";
+        }
+    }
+}
