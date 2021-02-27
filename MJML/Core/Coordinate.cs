fa02095725a6c696729572b3b62004224918548c
Liam Riddell <web@liamriddell.co.uk>
2021-02-27 15:43:33 +0000

    QoL Fixes

--- MJML/Core/Coordinate.cs
+++ MJML/Core/Coordinate.cs
@@ -11,16 +11,4 @@
             Y = y;
         }
     }
-
-    public class CssCoordinate
-    {
-        public string X { get; set; }
-        public string Y { get; set; }
-
-        public CssCoordinate(string x, string y)
-        {
-            X = x;
-            Y = y;
-        }
-    }
 }
---
+++ MJML/Core/Css/CssCoordinate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MjmlDotNet.Core.Css
+{
+    public class CssCoordinate
+    {
+        public string X { get; set; }
+        public string Y { get; set; }
+
+        public CssCoordinate(string x, string y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
